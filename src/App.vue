0b6a8688c3b07f9e84f35b7d4891fbb185aa5471
Elbo <Becarek@gmail.com>
2024-08-06 17:58:58 +0200

    Nainstalován router

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,69 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <SideBar></SideBar>
+  <MainField
+  v-if="kontejnerActive"></MainField>
+  <SeznamKontejneru 
+    v-if="seznamActive"
+    ></SeznamKontejneru>
 </template>
 
+<script>
+
+import SideBar from './components/SideBar.vue';
+import MainField from './components/MainField.vue';
+import SeznamKontejneru from './components/SeznamKontejneru.vue';
+import { reactive, toRefs, onMounted } from 'vue'
+  
+  export default {
+    name: 'App',
+  components: {
+    SideBar,
+    MainField,
+    SeznamKontejneru
+  },
+    setup () {
+      const state = reactive({
+        
+        seznamActive: "",
+        kontejnerActive: true
+      })
+      onMounted(() => {
+        window.eventBus.on("seznam-clicked", event => {
+          state.kontejnerActive = event.kontejnerActive;
+          state.seznamActive = event.seznamActive;
+          
+          
+        })
+        window.eventBus.on("kontejner-page-clicked", even => {
+          state.kontejnerActive = even.kontejnerActive;
+          state.seznamActive = even.seznamActive;
+        
+        })
+        
+        
+      });
+    
+      return {
+        ...toRefs(state),
+      }
+    }
+  }
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: 0;
+  padding: 0;
+  font-family: "Poppins", sans-serif;
 }
-
-nav {
-  padding: 30px;
+#app{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+h1{
+  font-size: 3rem;
 }
 </style>
---
+++ src/seznamKontejneru.js
@@ -0,0 +1,48 @@
+export const data = [{
+  id:1,
+  umisteniKontejneru: "MSN001P5115482",
+  cisloKontejneru: "MEDU 56845-7",
+  typKontejneru: "HC",
+    velikostKontejneru: 40,
+    popUp: true
+},
+{
+  id:2,
+  umisteniKontejneru: "MSN101P3115452",
+  cisloKontejneru: "AIUP 59218-5",
+  typKontejneru: "HC",
+  velikostKontejneru: 20,
+  popUp: false
+},
+{
+  id:3,
+  umisteniKontejneru: "MSN208P514882",
+  cisloKontejneru: "TUIO 31827-5",
+  typKontejneru: "OT",
+  velikostKontejneru: 40,
+  popUp: true
+},
+{
+  id:4,
+  umisteniKontejneru: "MSN098P5135487",
+  cisloKontejneru: "TRLU 81792-8",
+  typKontejneru: "HC",
+    velikostKontejneru: 40,
+    popUp: false
+},
+{
+  id:5,
+  umisteniKontejneru: "MSN191P3118432",
+  cisloKontejneru: "MEDV 18627-9",
+  typKontejneru: "DV",
+  velikostKontejneru: 20,
+  popUp: false
+},
+{
+  id:6,
+  umisteniKontejneru: "MSN208P514882",
+  cisloKontejneru: "THBT 92647-8",
+  typKontejneru: "OT",
+  velikostKontejneru: 40,
+  popUp: false
+}]
